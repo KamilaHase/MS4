e85f3139ce9b0b5cea275823ac3070302d7b3226
KamilaHase <k.rabinakova@gmail.com>
2022-01-29 20:01:59 +0000

    Deleted app contact in order to start it again and fix it.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
+""" Renders Models to the Admin View"""
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Product, Category, Brand
 
+""" Renders Models to the Admin View"""
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
