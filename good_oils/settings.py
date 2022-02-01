54de9124b19d1bbc232e62e0f6c44521a291b2d5
KamilaHase <k.rabinakova@gmail.com>
2022-02-01 23:45:43 +0000

    Corrected ALLOWED_HOSTS adress for heroku.

--- good_oils/settings.py
+++ good_oils/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['good-oils.heroku.app.com', 'localhost']
+ALLOWED_HOSTS = ['good-oils.herokuapp.com', 'localhost']
 
 
 # Application definition
