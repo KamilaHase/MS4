6e1b63ad1d6886eaf86a9e7806d19b2e89691de5
KamilaHase <k.rabinakova@gmail.com>
2022-02-05 15:45:06 +0000

    Added functionality to send real emails and saved.

--- good_oils/settings.py
+++ good_oils/settings.py
@@ -223,8 +223,6 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = ''
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
