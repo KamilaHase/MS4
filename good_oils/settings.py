df40a6e5912f5897ccfaac981cafe3c81f71f82e
KamilaHase <k.rabinakova@gmail.com>
2022-02-06 14:50:08 +0000

    Corrected AUTH_PASSWORD_VALIDATOR

--- good_oils/settings.py
+++ good_oils/settings.py
@@ -144,9 +144,9 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        ('django.contrib.auth\
-        .password_validation.UserAttributeSimilarityValidator'),
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'),
     },
     {
         'NAME':
